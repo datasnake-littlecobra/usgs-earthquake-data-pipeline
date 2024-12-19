af665627f449844e0dbb804f743e1048e74511bd
datasnake-littlecobra <prabhakar.sharma@datasnake.io>
2024-12-19 15:51:45 -0600

    ingesting usgs with dynamic time ranges

--- usgs-earthquake-data-ingest-dynamic.py
+++ usgs-earthquake-data-ingest-dynamic.py
@@ -87,11 +87,11 @@ def fetch_earthquake_data_time(API_URL: str, start_time: str, end_time: str) -> 
         logging.info(f"Error fetching data from API: {e}")
         return {}
 
-def fetch_earthquake_data_time_and_limit(API_URL: str, start_time: str, end_time: str, limit: int) -> dict:
+def fetch_earthquake_data_time_and_limit_offset(API_URL: str, start_time: str, end_time: str, limit: int, offset: int) -> dict:
     """Fetch earthquake data from the USGS API."""
     try:
-        params = {"format": "geojson", "starttime": start_time, "endtime": end_time, "limit": limit}
-        logging.info(f"url: {API_URL} with starttime: {start_time} and endtime: {end_time}")
+        params = {"format": "geojson", "starttime": start_time, "endtime": end_time, "limit": limit, "offset": offset}
+        logging.info(f"url: {API_URL} with starttime: {start_time} and endtime: {end_time} with offset: {offset}")
         response = requests.get(API_URL, params=params)
         response.raise_for_status()  # Raise HTTPError for bad responses
         return response.json()
@@ -238,24 +238,53 @@ def fetch_data_by_limit_range(api_url: str, start_year: int, end_year: int, limi
         logging.info(f"intital start date iso: {start_time_iso} and end date: {end_time_iso}")
         logging.info(f"limit: {limit}")
         
-        total_data = 52000
-        total_count_so_far = 0
+        # total_data = 52000
+        # total_count_so_far = 0
         offset = 1
         
         while True:
-            
             logging.info(f"call api with limit: {limit} with offset: {offset}")
-            # data = fetch_earthquake_data_time_and_limit(api_url, start_time_iso, end_time_iso, limit)
-            logging.info(f"Started with total data: {total_data}")
+            data = fetch_earthquake_data_time_and_limit_offset(api_url, start_time_iso, end_time_iso, limit, offset)
+            features = data.get("features",[])
+            # logging.info(f"Started with total data: {total_data}")
+            if "features" not in data or not data["features"]:
+                logging.info(f"No more data available for {start_time_iso} to {end_time_iso} at offset {offset}.")
             
+            if data and "features" in data:
+                # all_data.extend(data["features"])
+                dataframe = parse_geojson_to_dataframe(data)
+                logging.info("--- dataframe.count() ---")
+                logging.info(dataframe.count())                
+                # Process the data (e.g., save or analyze it)
+                logging.info(f"Fetched {len(data.get('features', []))} records.")
+                logging.info("Parsing geojson dataframe back from api call...")
+                logging.info("Saving the dataframe to CSV...")
+                save_to_csv(dataframe, output_dir)
+                logging.info("Saving the dataframe to JSON...")
+                save_to_json(dataframe, output_dir)
+                logging.info("Saving the dataframe to local delta lake...")
+                save_to_delta_table(dataframe, delta_dir, mode="append")
+                logging.info("Uploading the delta lake to Object Storage...")
+                # need research on appending vs overwrite
+                # z order and other ways to make it efficient
+                # upload_delta_to_s3(delta_dir, bucket_name, delta_s3_key)
+                logging.info("Finished with Files...")
+                logging.info("Going to call Cassandra Connect with:")
+                logging.info(cluster_ips)
+                logging.info(keyspace)
+                save_to_cassandra_main(cluster_ips, keyspace, table_name, dataframe, batch_size, timeout)
+    
             offset += limit
             logging.info(f"use limit: {limit} with updated offset: {offset}")
             
-            total_data -= offset
-            logging.info(f"Total data left: {total_data}")
+            # total_data -= limit
+            # logging.info(f"Total data left: {total_data}")
             
-            if total_data < limit:
-                logging.info(f"total data: {total_data} < {limit}, so exit")
+            # if total_data < limit:
+            #     logging.info(f"total data: {total_data} < {limit}, so exit")
+            #     break
+            
+            if len(features) < limit:
                 break
             
         logging.info(f"done reading all data for start time: {start_time_iso} to end time: {end_time_iso}")
